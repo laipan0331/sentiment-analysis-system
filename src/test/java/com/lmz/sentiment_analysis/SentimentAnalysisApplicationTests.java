74dbe2b21492a878901d47016e88d5c0082a396a
Panpan Lai <lai.pa@northeastern.edu>
2026-02-21 00:54:24 -0800

    fix: enable test profile for integration tests to use H2 database

--- src/test/java/com/lmz/sentiment_analysis/SentimentAnalysisApplicationTests.java
+++ src/test/java/com/lmz/sentiment_analysis/SentimentAnalysisApplicationTests.java
@@ -2,8 +2,10 @@ package com.lmz.sentiment_analysis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SentimentAnalysisApplicationTests {
 	//Tests if the application context loads without any issues.
 	//If the context fails to load, this test will fail.
